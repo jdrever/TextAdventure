83d64ea10634cea3b6778d63f62d3ea366eb835c
jamesdrever <james@jamesdrever.co.uk>
2015-12-05 13:45:29 +0000

    changes to parser

---
+++ TextAdventure.Application/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventure.Interface;
+
+namespace TextAdventure.Application
+{
+    public class Parser : IParser
+    {
+        private ActionCoordinator _actionCoordinator;
+
+        public Parser(ActionCoordinator actionCoordinator)
+        {
+            
+            if (actionCoordinator == null) throw new ArgumentNullException("actionCoordinator");
+            _actionCoordinator = actionCoordinator;
+        }
+
+        public string ParseInput(string input)
+        {
+            string[] words = input.Split(' ');
+            string firstWords = words[0].ToUpper();
+
+            if (firstWords == "TAKE")
+            {
+                // need location/character
+                return _actionCoordinator.Take(words[0], null, null).Message;
+            }
+            return "I didn't understand!";
+        }
+    }
+}
